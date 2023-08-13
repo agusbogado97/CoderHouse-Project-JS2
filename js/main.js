866440e9ed093ecd8c933811ae8c7534490d1cb5
agusbogado97 <agus.y.roger@gmail.com>
2023-08-12 22:33:59 -0300

    carrito de compras, aplicación del DOM, Ternarios, JSON y Storage.

--- js/main.js
+++ js/main.js
@@ -7,48 +7,64 @@ function agregarAlCarrito(nombre, precio, cantidad) {
         cantidad: cantidad
     };
     carritoDeCompras.push(producto);
-    }
-
+}
 
 agregarAlCarrito("Remera", 2500, 2);
 agregarAlCarrito("Pantalon", 8000, 1);
 agregarAlCarrito("Medias", 500, 4);
 
 function calcularTotal() {
-    let total = 0;
-    for (const producto of carritoDeCompras) {
-        total += producto.precio * producto.cantidad;
-    }
-    return total;
-    }
+    return carritoDeCompras.reduce((total, producto) =>
+        total + producto.precio * producto.cantidad, 0);
+}
 
 function mostrarCarrito() {
     console.log("Productos en el carrito:");
-    for (const producto of carritoDeCompras) {
-        console.log(`${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`);
-    }
+    carritoDeCompras.forEach(producto =>
+        console.log(`${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`));
     console.log("Total del carrito: " + calcularTotal());
 }
 
 function buscarProducto(nombreProducto) {
-    const productoEncontrado = carritoDeCompras.find(producto =>
-        producto.nombre.toLowerCase() == nombreProducto.toLowerCase());
-    return productoEncontrado;
+    return carritoDeCompras.find(producto =>
+        producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
 }
 
 function mostrarInformacionProducto(producto) {
-    if (producto) {
-        console.log("Información del producto encontrado:");
-        console.log(`${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`);
-    } else {
-        console.log("Producto no encontrado en el carrito.");
-    }
+    producto
+        ? console.log(`Información del producto encontrado:\n${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`)
+        : console.log("Producto no encontrado en el carrito.");
 }
 
+// Referencias a elementos del DOM
+const inputBusqueda = document.getElementById('buscarProducto');
+const botonBuscar = document.getElementById('botonBuscar');
+const resultadoBusqueda = document.getElementById('resultadoBusqueda');
+
+// Agrega evento al botón de búsqueda
+botonBuscar.addEventListener('click', () => {
+    const nombreProductoABuscar = inputBusqueda.value.toLowerCase();
+    const productoEncontrado = buscarProducto(nombreProductoABuscar);
+
+    if (productoEncontrado) {
+        const imagenSrc = `${productoEncontrado.nombre.toLowerCase()}.jpg`;
+        const resultadoHTML = `
+            <div class="producto-card">
+                <div class="producto-imagen">
+                    <img src="${imagenSrc}" alt="${productoEncontrado.nombre}">
+                </div>
+                <div class="producto-info">
+                    <p>Nombre: ${productoEncontrado.nombre}</p>
+                    <p>Precio: ${productoEncontrado.precio}</p>
+                    <p>Cantidad: ${productoEncontrado.cantidad}</p>
+                </div>
+            </div>
+        `;
+        resultadoBusqueda.innerHTML = resultadoHTML;
+    } else {
+        resultadoBusqueda.innerHTML = 'Producto no encontrado en el carrito.';
+    }
+});
+
+// Mostrar el carrito inicial y agregar aquí otras llamadas necesarias
 mostrarCarrito();
-
-const nombreProductoABuscar = prompt("Ingrese el nombre del producto que desea buscar:");
-const productoEncontrado = buscarProducto(nombreProductoABuscar.toLowerCase());
-mostrarInformacionProducto(productoEncontrado);
-
-//Carrito de compras, con la finalidad de mostrar el producto buscado, su nombre, precio y cantidad de stock.
