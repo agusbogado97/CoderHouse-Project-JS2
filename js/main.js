5b9bf7fa874108ffc6d0d11e8d037412b29d0712
agusbogado97 <agus.y.roger@gmail.com>
2023-08-13 10:22:06 -0300

    JSON y Storage

--- js/main.js
+++ js/main.js
@@ -1,70 +1,70 @@
+
 const carritoDeCompras = [];
 
-function agregarAlCarrito(nombre, precio, cantidad) {
+const agregarAlCarrito = (nombre, precio, cantidad) => {
     const producto = {
-        nombre: nombre,
-        precio: precio,
-        cantidad: cantidad
+        nombre,
+        precio,
+        cantidad,
     };
     carritoDeCompras.push(producto);
-}
+};
 
-agregarAlCarrito("Remera", 2500, 2);
-agregarAlCarrito("Pantalon", 8000, 1);
-agregarAlCarrito("Medias", 500, 4);
+const calcularTotal = () =>
+    carritoDeCompras.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
 
-function calcularTotal() {
-    return carritoDeCompras.reduce((total, producto) =>
-        total + producto.precio * producto.cantidad, 0);
-}
-
-function mostrarCarrito() {
+const mostrarCarrito = () => {
     console.log("Productos en el carrito:");
     carritoDeCompras.forEach(producto =>
         console.log(`${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`));
     console.log("Total del carrito: " + calcularTotal());
-}
+};
 
-function buscarProducto(nombreProducto) {
-    return carritoDeCompras.find(producto =>
+const buscarProducto = nombreProducto =>
+    carritoDeCompras.find(producto =>
         producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
-}
 
-function mostrarInformacionProducto(producto) {
-    producto
-        ? console.log(`Información del producto encontrado:\n${producto.nombre} - Precio: ${producto.precio} - Cantidad: ${producto.cantidad}`)
-        : console.log("Producto no encontrado en el carrito.");
-}
+const mostrarInformacionProducto = producto => {
+    const imagenSrc = producto
+        ? `${producto.nombre.toLowerCase()}.jpg`
+        : '';
+    const resultadoHTML = `
+        <div class="producto-card">
+            <div class="producto-imagen">
+                ${producto ? `<img src="${imagenSrc}" alt="${producto.nombre}">` : ''}
+            </div>
+            <div class="producto-info">
+                ${producto
+                        ?`<p>Nombre: ${producto.nombre}</p>
+                        <p>Precio: ${producto.precio}</p>
+                        <p>Cantidad: ${producto.cantidad}</p>`
+                    : "Producto no encontrado en el carrito."}
+            </div>
+        </div>
+    `;
+    resultadoBusqueda.innerHTML = resultadoHTML;
+};
 
-// Referencias a elementos del DOM
-const inputBusqueda = document.getElementById('buscarProducto');
-const botonBuscar = document.getElementById('botonBuscar');
-const resultadoBusqueda = document.getElementById('resultadoBusqueda');
+const inputBusqueda = document.getElementById("buscarProducto");
+const botonBuscar = document.getElementById("botonBuscar");
+const resultadoBusqueda = document.getElementById("resultadoBusqueda");
 
-// Agrega evento al botón de búsqueda
-botonBuscar.addEventListener('click', () => {
+botonBuscar.addEventListener("click", () => {
     const nombreProductoABuscar = inputBusqueda.value.toLowerCase();
     const productoEncontrado = buscarProducto(nombreProductoABuscar);
-
-    if (productoEncontrado) {
-        const imagenSrc = `${productoEncontrado.nombre.toLowerCase()}.jpg`;
-        const resultadoHTML = `
-            <div class="producto-card">
-                <div class="producto-imagen">
-                    <img src="${imagenSrc}" alt="${productoEncontrado.nombre}">
-                </div>
-                <div class="producto-info">
-                    <p>Nombre: ${productoEncontrado.nombre}</p>
-                    <p>Precio: ${productoEncontrado.precio}</p>
-                    <p>Cantidad: ${productoEncontrado.cantidad}</p>
-                </div>
-            </div>
-        `;
-        resultadoBusqueda.innerHTML = resultadoHTML;
-    } else {
-        resultadoBusqueda.innerHTML = 'Producto no encontrado en el carrito.';
-    }
+    mostrarInformacionProducto(productoEncontrado);
 });
 
-// Mostrar el carrito inicial y agregar aquí otras llamadas necesarias
+const guardarCarritoEnLocalStorage = () => {
+    localStorage.setItem("carrito", JSON.stringify(carritoDeCompras));
+};
+
+const carritoGuardado = JSON.parse(localStorage.getItem("carrito"));
+if (carritoGuardado) {
+    carritoDeCompras.push(...carritoGuardado);
+}
+
+window.addEventListener("beforeunload", guardarCarritoEnLocalStorage);
+
 mostrarCarrito();
+
